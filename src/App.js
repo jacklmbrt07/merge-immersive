14fd48e15ba5ab703b722e92fb021fcdc1d412f9
Johnathan Blackburn <jhblackburn90@gmail.com>
2020-08-19 17:40:14 -0700

    cloned repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <h2>Group A</h2>
       </div>
     );
   }
