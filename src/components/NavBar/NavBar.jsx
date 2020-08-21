a05671adab11057e5f5a2c29003e720de36ff48c
Johnathan Blackburn <jhblackburn90@gmail.com>
2020-08-21 16:47:35 -0700

    preparing nav for login/logout features

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = (props) => {
-  const [open, setOpen] = useState();
+  // const [open, setOpen] = useState();
   return (
     <nav className="navbar">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a href="#" className="icon-button"></a>
-          <a
-            href="#"
-            className="icon-button"
-            onClick={() => setOpen(!open)}
-          ></a>
-          <a href="#" className="icon-button"></a>
+          <a href="#" className="icon-button">
+            Login
+          </a>
         </li>
       </ul>
     </nav>
   );
 };
+
+// const NavBar = (props) => {
+//   let nav = props.user ? (
+//     <div>
+//       <Link to="" onClick={props.handleLogout}>
+//         {" "}
+//         LOG OUT
+//       </Link>
+//       &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+//       <span>WELCOME, {props.user.name}</span>
+//     </div>
+//   ) : (
+//     <div>
+//       <Link to="/login">LOG IN</Link>
+//       &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+//       <Link to="/signup">SIGN UP</Link>
+//     </div>
+//   );
+//   return <div>{nav}</div>;
+// };
 
 export default NavBar;
