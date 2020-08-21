c0649abcfe1daf095f55246c066226491ba3d3cb
Johnathan Blackburn <jhblackburn90@gmail.com>
2020-08-21 16:23:00 -0700

    start of navbar and styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const [open, setOpen] = useState();
   return (
-    <div>
-      <p>Header</p>
-    </div>
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="#" className="icon-button"></a>
+          <a
+            href="#"
+            className="icon-button"
+            onClick={() => setOpen(!open)}
+          ></a>
+          <a href="#" className="icon-button"></a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
