5accf99a80af47345f886fc329e06be6a188d22e
jacklmbrt07 <jacklmbrt@gmail.com>
2020-08-22 09:05:28 -0700

    changed filenames

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/user');
+const usersCtrl = require('../../controllers/users');
 
 /*---------- Public Routes ----------*/
 router.post('/signup', usersCtrl.signup);
