83c34922e15fe7e817279ef80317310014d255ab
jacklmbrt07 <jacklmbrt@gmail.com>
2020-08-21 18:21:24 -0700

    changed routesapi name

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,59 +1,71 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './LoginPage.css';
-import userService from '../../utils/userService';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+import userService from "../../utils/userService";
 
 class LoginPage extends Component {
+  state = {
+    email: "",
+    pw: "",
+  };
 
-    state = {
-        email: '',
-        pw: ''
-    };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await userService.signup(this.state);
+      this.props.handleSignupOrLogin();
+      this.props.history.push("/");
+    } catch (err) {
+      alert("Invalid Credentials!!!");
     }
+  };
 
-    handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await userService.signup(this.state);
-            this.props.handleSignupOrLogin();
-            this.props.history.push('/');
-
-        } catch (err) {
-            alert('Invalid Credentials!!!');
-        }
-    }
-
-
-    render() {
-        return (
-            <div className="LoginPage">
-                <header className="header-footer">Log In</header>
-                <form className="form-horizontal" onSubmit={this.handleSubmit} >
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12 text-center">
-                            <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
-              <Link to='/'>Cancel</Link>
-                        </div>
-                    </div>
-                </form>
+  render() {
+    return (
+      <div className="LoginPage">
+        <header className="header-footer">Log In</header>
+        <form className="form-horizontal" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                value={this.state.email}
+                name="email"
+                onChange={this.handleChange}
+              />
             </div>
-        );
-    }
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                value={this.state.pw}
+                name="pw"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12 text-center">
+              <button className="btn btn-default">Log In</button>
+              &nbsp;&nbsp;&nbsp;
+              <Link to="/">Cancel</Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginPage;
