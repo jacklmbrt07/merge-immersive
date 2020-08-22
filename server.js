f7d6dc797b4bf2d71b9a1e2c20546aaa5d4488d0
hosead6168 <dominique11h@yahoo.com>
2020-08-22 11:00:57 -0500

    Route correction

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/api/users', require('./routes/api/user'));
+app.use('/api/users', require('./routes/api/users'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
