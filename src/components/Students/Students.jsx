7c185fc94d07798a9100b3ff19a75863482a2be1
Johnathan Blackburn <jhblackburn90@gmail.com>
2020-08-25 10:33:20 -0700

    profile start

--- src/components/Students/Students.jsx
+++ src/components/Students/Students.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 const Students = (props) => {
     const { user } = props
--- src/pages/UserDetail/UserDetail.jsx
+++ src/pages/UserDetail/UserDetail.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "../../components/NavBar/NavBar";
-import Students from "../../components/Students/Students";
+// import Students from "../../components/Students/Students";
+import "../UserDetail/UserDetail.css"
 
 class UserDetail extends Component {
   state = {
@@ -14,10 +15,30 @@ class UserDetail extends Component {
 
       <>
         <NavBar />
-        <div>
-          <p>Profile</p>
-          <Students />
-        </div>
+        <section className="feed">
+          <div className="container">
+            <div className="feed-container">
+              <div className="profile-card2">
+                <div className="card-header2">
+                  <span>
+                    <h2>Johnathan</h2>
+                  </span>
+                  <p>SEI Immersive Student</p>
+                  <p>Class: 629</p>
+                </div>
+                <img className="image2"
+                  src="https://avatars2.githubusercontent.com/u/25213510?s=460&u=ccd76bfb4349453011a6f276200dbcf087be71c2&v=4"
+                  alt="profile" />
+                <div className="card-info2">
+                  <p>Profile Info</p>
+                </div>
+              </div>
+              <div className="card-container">
+
+              </div>
+            </div>
+          </div>
+        </section>
       </>
     );
   }
